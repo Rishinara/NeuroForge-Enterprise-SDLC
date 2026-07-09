309de29be7747b7b819b084f29c306111bd30931
MENDEM RISHITHA <mendemrishitha@gmail.com>
2026-07-09 16:25:33 +0530

    Completed Authentication Module

---
+++ src/main/java/com/neuroforge/backend/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.neuroforge.backend.controller;
+
+import com.neuroforge.backend.dto.LoginRequest;
+import com.neuroforge.backend.dto.RegisterRequest;
+import com.neuroforge.backend.entity.User;
+import com.neuroforge.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest request) {
+        return userService.registerUser(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return userService.loginUser(request);
+    }
+}
